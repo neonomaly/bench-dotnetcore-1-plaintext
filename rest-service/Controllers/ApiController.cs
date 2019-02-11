5451382598a079b8a14b6d035b74d43f0b0145fd
Mikhail Kadysev <neonomaly.x@gmail.com>
2019-02-11 16:22:28 +0300

    Hello world in rest

---
+++ rest-service/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rest_service.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get([FromQuery(Name = "my-name")] string name)
+        {
+            return name == null ? "Please, setup query parameter \"my-name\"" : $"Hello, {name}!";
+        }
+
+    }
+}
